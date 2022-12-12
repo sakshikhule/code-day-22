40e2718831070c8993f4f2a4d176f1185db59257
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2022-12-12 16:53:09 +0530

    Add files via upload

---
+++ day 22.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int sum(int n)
+{
+   int i, Prime = 1;
+   for(i = 2; i <= n/2; ++i)
+      {
+         if(n % i == 0)
+         {
+            Prime = 0;
+            break;
+         }
+      }
+      return Prime;
+}
+int main()
+{
+    int num, i;
+    cout<<"Insert the num: ";
+    cin>>num;
+    int flag = 0;
+    for(i = 2; i <= num/2; ++i)
+        {
+           if(sum(i) == 1)
+           {
+               if(sum(num-i) == 1)
+               {
+                  cout<<num<<" can be expressed as the sum of "<<i<<" and "<<num-i;
+                  flag = 1;
+               }
+            }
+        }
+        if(flag == 0)
+        cout<<num<<" cannot be expressed as the sum of two primes";
+ return 0;
+}
